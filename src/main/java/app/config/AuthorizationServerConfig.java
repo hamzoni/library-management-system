63991bc442770b45142c591b16b6d83d4a3c009b
Ta Quy (FSU1.BU91) <QuyT@fsoft.com.vn>
2018-08-18 09:22:17 +0700

    merge Oauth2client and AuthServer configuration

--- src/main/java/app/config/AuthorizationServerConfig.java
+++ src/main/java/app/config/AuthorizationServerConfig.java
@@ -1,21 +1,37 @@
 package app.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
+@EnableOAuth2Client
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+	@Value("${spring.data.rest.base-path}")
+	private String BASE_URL;
+	
 	@Value("${oauth.lms.client-id}")
 	private String CLIENT_ID;
 	
@@ -67,4 +83,36 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.accessTokenValiditySeconds(ACCESS_TOKEN_LIFESPAN)
 			.refreshTokenValiditySeconds(REFRESH_TOKEN_LIFESPAN);
 	}
+	
+	// Access token request configuration
+	public OAuth2ProtectedResourceDetails resource(String username, String password) {
+		
+	    ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+	    
+	    List<String> scopes = Arrays.asList("read", "write", "trust");
+	    
+		resource.setAccessTokenUri(BASE_URL + "/oauth/token");
+		resource.setClientId(CLIENT_ID);
+		resource.setClientSecret(SECRET);
+		resource.setGrantType(GRANT_TYPE_PASSWORD);
+		resource.setScope(scopes);
+		
+		resource.setUsername(username);
+		resource.setPassword(password);
+		
+		return resource;
+	}
+
+	/*
+	 * Usage: restTemplate.getAccessToken();
+	 */
+	@Bean
+    public OAuth2RestOperations restTemplate(String username, String password) {
+        AccessTokenRequest atr = new DefaultAccessTokenRequest();
+        DefaultOAuth2ClientContext ctx = new DefaultOAuth2ClientContext(atr);
+        OAuth2ProtectedResourceDetails details = resource(username, password);
+
+        return new OAuth2RestTemplate(details, ctx);
+    }
+	
 }
