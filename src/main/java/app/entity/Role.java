7e93b4a5012c2c3508a69895f598a375141ee424
Ta Quy (FSU1.BU91) <QuyT@fsoft.com.vn>
2018-08-18 13:27:22 +0700

    app cleaning

--- src/main/java/app/entity/Role.java
+++ src/main/java/app/entity/Role.java
@@ -1,13 +1,8 @@
 package app.entity;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
--- src/main/java/app/service/AuthService.java
+++ src/main/java/app/service/AuthService.java
@@ -1,8 +1,5 @@
 package app.service;
 
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-
 import app.dto.LoginDto;
 import app.dto.UserDto;
 import app.entity.User;
