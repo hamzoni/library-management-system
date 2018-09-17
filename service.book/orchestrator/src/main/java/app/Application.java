5321896e7ee6d9e51957cfaf7ff5fcf46143ac9d
taquy.se <taquy.se@gmail.com>
2018-09-17 07:33:33 +0700

    build book.service.orchestrator skeleton

--- service.book/orchestrator/src/main/java/app/Application.java
+++ service.book/orchestrator/src/main/java/app/Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableSpringDataWebSupport
 public class Application {
 
 	public static void main(String[] args) {
---
+++ service.book/orchestrator/src/main/java/app/broker/BrokerService.java
@@ -0,0 +1,37 @@
+package app.broker;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate.RabbitConverterFuture;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class BrokerService {
+	@Autowired
+	private RabbitTemplate template;
+
+	@Autowired
+	private AsyncRabbitTemplate asyncTpl;
+
+	@Autowired
+	private DirectExchange direct;
+
+	@Scheduled(fixedDelay = 1000, initialDelay = 500)
+	public Message sendSync(Message message) {
+
+		String route = message.getRoute();
+
+		return (Message) template.convertSendAndReceive(direct.getName(), route, message);
+
+	}
+
+	@Scheduled(fixedDelay = 1000L)
+	public RabbitConverterFuture<Message> sendAsync(Message message) {
+
+		String route = message.getRoute();
+
+		return asyncTpl.convertSendAndReceive(direct.getName(), route, message);
+
+	}
+}
