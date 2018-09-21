64b9e1fc4b222e6125ac34429ac75e3fd3156b9f
taquy.se <taquy.se@gmail.com>
2018-09-21 10:45:30 +0700

    add zuul config

--- api.gateway/src/main/java/api/gateway/Application.java
+++ api.gateway/src/main/java/api/gateway/Application.java
@@ -2,11 +2,37 @@ package api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import api.gateway.filters.ErrorFilter;
+import api.gateway.filters.PostFilter;
+import api.gateway.filters.PreFilter;
+import api.gateway.filters.RouteFilter;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
 	}
 }
